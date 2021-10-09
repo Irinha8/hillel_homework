2068e64979ad7699feeadaccdc1666c7a402aeb7
Irinha8 <irinha.aspirant@gmail.com>
2021-10-09 14:53:17 +0300

    guess number game

---
+++ gn2.py
@@ -0,0 +1,11 @@
+from random import randint
+counter = 0
+while answer != "Y":
+    if
+    print(f"Is {x} guessed number?")
+    x = numbers[0]
+    answer = input()
+    counter += 1
+    numbers.pop(0)
+    if answer == "Y":
+        print(f"You've used {counter} tries!")
