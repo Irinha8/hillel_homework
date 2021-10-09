670efbc3ad812bd9fdc9ca6fbe5c1a731e69a4b4
Irinha8 <90479336+Irinha8@users.noreply.github.com>
2021-10-09 14:50:05 +0300

    Update guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -5,12 +5,12 @@ while len(numbers) < 100:
     x = randint(1, 100)
     if x not in numbers:
         numbers.append(x) # cоздала список случайных чисел от 0 до 100, которые не повторяются
-answer = "N"
-while answer != "Y":
+answer = "no"
+while answer != "yes":
     print(f"Is {x} guessed number?")
     x = numbers[0]
     answer = input()
     counter += 1
     numbers.pop(0)
-if answer == "Y":
+if answer == "yes":
     print(f"You've used {counter} tries!")
