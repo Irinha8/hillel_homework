1b96a2dcd3b2c3b630b898af10be30341c6f1457
Irinha8 <irinha.aspirant@gmail.com>
2021-10-09 14:14:58 +0300

    guess number game

---
+++ guess_number.py
@@ -0,0 +1,22 @@
+from random import randint
+counter = 0
+numbers = []
+while len(numbers) < 101:
+    x = randint(0, 100)
+    if x not in numbers:
+        numbers.append(x) # список случайных чисел от 0 до 100, которые не повторяются
+answer = "N"
+while answer != "Y":
+    print(f"Is {x} guessed number?")
+    x = numbers[0]
+    answer = input()
+    counter += 1
+    numbers.pop(0)
+    if answer == "Y":
+        print(f"You've used {counter} tries!")
+
+
+
+
+
+
