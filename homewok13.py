95898bdc181e438968da8a7e0d9e13af09a43692
Irinha8 <irinha.aspirant@gmail.com>
2021-10-22 18:18:10 +0300

    закрыла файлы

--- homewok13.py
+++ homewok13.py
@@ -11,7 +11,9 @@ reader = open(path, mode="r")
 content = reader.read()
 numbers = sorted([int(number) for number in content.split()])
 string_numbers = " ".join(map(str, numbers))
+reader.close()
 path = "./out.txt"
 writer = open(path, mode='w')
 result = writer.write(string_numbers)
+writer.close()
 
