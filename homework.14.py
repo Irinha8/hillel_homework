657d55602d218249746fe91313366203c4b835cd
Irinha8 <90479336+Irinha8@users.noreply.github.com>
2021-10-22 18:38:47 +0300

    Update homework.14.py

--- homework.14.py
+++ homework.14.py
@@ -1,11 +1,4 @@
-""""
-Напишите программа, которая считывает слова из файла 'in.txt", фильтрует все длина которых больше 5 и пишет их в файл
-"out.txt".
-Пример входных данных в файле:
-a ab abc abcd abcde abcdef
-Пример выходных данных в файле:
-abcde abcdef
-"""
+
 path = "./in.txt"
 reader = open(path, mode="r")
 content = reader.read()
