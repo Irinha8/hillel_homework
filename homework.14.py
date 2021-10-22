220f9979b276c9d826cc5e5a35f5e49a17e2c1ab
Irinha8 <irinha.aspirant@gmail.com>
2021-10-22 18:37:20 +0300

    фильтр слов

--- homework.14.py
+++ homework.14.py
@@ -9,3 +9,15 @@ abcde abcdef
 path = "./in.txt"
 reader = open(path, mode="r")
 content = reader.read()
+words = [word for word in content.split()]
+res = " ".join(list(filter(lambda word: len(word) >= 5, words)))
+reader.close()
+path = "./out.txt"
+writer = open(path, mode='w')
+result = writer.write(res)
+writer.close()
+
+
+
+
+
